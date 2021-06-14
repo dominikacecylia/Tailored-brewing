6ded753c4649dfd6f09141fa319a2cd34838b8ac
dominikacecylia <dominikacecylia@users.noreply.github.com>
2021-06-14 17:39:08 +0100

    Cleans the file

--- fm_optim.py
+++ fm_optim.py
@@ -4,6 +4,5 @@ def calculate_prevalence(condition_positive, total_population):
 def calculate_m_factor(prevalence, cost_negative, cost_positive):
     return ((1-prevalence)/prevalence)*(cost_negative/cost_positive)
 
-
 def calculate_fm(tpr, fpr, m):
     return tpr - (m*fpr)
